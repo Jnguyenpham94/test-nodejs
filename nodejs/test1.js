7bee7e1440cd90474623d503fe69ae9316e872fd
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-05-28 20:57:23 -0700

    function and if testing

--- nodejs/test1.js
+++ nodejs/test1.js
@@ -2,4 +2,19 @@ console.log("-----------------");
 console.log("HELLO WORLD");
 console.log("-----------------");
 
-console.log(typeof 5);
+var x = true;
+console.log(x);
+
+if (x) {
+    truthy();
+} else {
+    falsy()
+}
+
+function truthy() {
+    console.log("It is TRUE");
+}
+
+function falsy() {
+    console.log("It is False");
+}
